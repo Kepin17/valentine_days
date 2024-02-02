5c0d4ce3509dbc2afa6d7d8c4d40180f778e50aa
Kevien.OJ <69498060+Kepin17@users.noreply.github.com>
2024-02-02 19:02:18 +0700

    Update index.jsx add emote

--- src/pages/MainContentPage/index.jsx
+++ src/pages/MainContentPage/index.jsx
@@ -19,7 +19,7 @@ const MainContentPage = () => {
     `Apa kabarmu hari ini, Baik?Pasti baik kan ya hahaha`,
     `Terimakasih sudah memilih ${maleName} menjadi orang spesial di dalam hidupmu`,
     `Aku tak sabar ingin segera bertemu dan jalan berdua denganmu hingga kita bisa hidup bersama`,
-    `Sebelumnya aku mau minta maaf kalau diriku mungkin pernah membuatmu kesal dan merasa kurang nyaman`,
+    `Sebelumnya aku mau minta maaf kalau diriku mungkin pernah membuatmu kesal😋`,
     `Aku cuman mau bilang , selamat hari valentine 2024 🎉`,
     `Di hari yang spesial ini kita berdua ingin melakukan kegiatan apa ?`,
     `Melakukan produktivitas atau rebahan seharian ? hahaha`,
